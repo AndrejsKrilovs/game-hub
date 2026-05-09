dd81264de99a22c4118c9d8ea641e565ca66ca95
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2026-05-09 07:50:40 +0300

    (feat): added kotlin plugin

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   kotlin("jvm") version "2.3.20"
+  kotlin("plugin.spring") version "2.3.20"
   id("org.springframework.boot") version "3.5.13"
   id("io.spring.dependency-management") version "1.1.7"
 }
@@ -8,6 +9,7 @@ group = "krilovs.andrejs.chess"
 version = "1.0-SNAPSHOT"
 
 dependencies {
+  implementation(kotlin("stdlib"))
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
